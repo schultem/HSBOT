a215f6e782dbde8dca6b5dbda190fdb4990fc798
mjs <schulte.watt@gmail.com>
2014-03-08 21:49:19 -0700

    Updating play logic and cleaning code.  Attack random minions and attack with random minions, so it at least has a chance of doing something good in some cases.  Using pre calculated images for character and stage as well as state.  Use win32gui to regain control of the window if it loses it.  Update pauses.

--- play.py
+++ play.py
@@ -4,13 +4,19 @@ import defines
 import logging
 from random import randint
 import os
+import win32gui
+import win32con
 
 src = None
+state_sigs = None
+character_sigs = None
+stage_sigs = None
 
 ###############
 #    FLAGS    #
 ###############
 NEW_GAME = False
+THE_COIN = False
 
 def desktop():
     pass
@@ -27,36 +33,32 @@ def versus():
 def select():
     global NEW_GAME
     NEW_GAME = True
+    
     actions.move_and_leftclick(c(defines.confirm_hand_button))
 def wait():
     pass
 def player():
-    global src
+    global src,character_sigs,stage_sigs
     global NEW_GAME
     actions.pause_pensively(1)
 
     if NEW_GAME:
         logging.info("-------------NEW GAME INFO--------------")
-        logging.info("OPPONENT: %s"%(vision.get_image_info('character',src,c(defines.enemy_box))))
-        logging.info("PLAYER  : %s"%(vision.get_image_info('character',src,c(defines.player_box))))
-        #logging.info("STAGE   : %s"%(vision.get_image_info('stage',src,c(defines.stage_box))))
-
-    NEW_GAME=False
-    
-    
-    player_ability = vision.color_range_reduced_mids(src,c(defines.reduced_ability_box),color='green')
-    player_attack  = vision.color_range_reduced_mids(src,c(defines.reduced_player_box),color='green')
-    player_minions = vision.color_range_reduced_mids(src,c(defines.reduced_player_minions_box),color='green',threshold=45)
-    player_cards   = vision.get_playable_cards(src,c(defines.hand_box))
+        logging.info("OPPONENT: %s"%(vision.get_image_info(src,character_sigs,c(defines.enemy_box))))
+        logging.info("PLAYER:   %s"%(vision.get_image_info(src,character_sigs,c(defines.player_box))))
+        #logging.info("STAGE:    %s"%(vision.get_image_info(src,stage_sigs,c(defines.stage_box))))
+        NEW_GAME=False
 
     #logging.info("------PLAY CARDS------")
+    src = vision.screen_cap()
+    player_cards   = vision.get_playable_cards(src,c(defines.hand_box))
     play_attempt_counter=0
     while(player_cards != []):
         if play_attempt_counter==2:
             break
-        actions.leftclick_move_and_leftclick(player_cards[0],c(defines.play_card_mid))
+        actions.leftclick_move_and_leftclick(player_cards[0],c(defines.play_card[randint(0,1)]))
         actions.move_and_leftclick(c(defines.neutral_minion))
-        actions.pause_pensively(1)
+        actions.pause_pensively(1.5)
         src = vision.screen_cap()
         player_cards   = vision.get_playable_cards(src,c(defines.hand_box))
         play_attempt_counter+=1
@@ -73,26 +75,26 @@ def player():
     src = vision.screen_cap()
     previous_player_minions=[]
     player_minions = vision.color_range_reduced_mids(src,c(defines.reduced_player_minions_box),color='green',threshold=45)
-    attack_attempt=0 #don't try to attack more than three times with the same minion
     while player_minions != [] and player_minions != None:
-        if previous_player_minions == player_minions:
-            if attack_attempt==2:
-                break
-            attack_attempt+=1
-        actions.move_and_leftclick(player_minions[0])
-        actions.move_cursor([player_minions[0][0],player_minions[0][1]+130])
+        p_m = randint(0,len(player_minions)-1) #attack with a random minion
+        actions.move_and_leftclick(player_minions[p_m])
+        actions.move_cursor([player_minions[p_m][0],player_minions[p_m][1]+130])
         actions.pause_pensively(0.35)
         src = vision.screen_cap()
         enemy=[]
         enemy.extend(vision.color_range_reduced_mids(src,c(defines.reduced_opponent_box1),color='red'))
         enemy.extend(vision.color_range_reduced_mids(src,c(defines.reduced_opponent_box2),color='red'))
         enemy_minions = vision.color_range_reduced_mids(src,c(defines.reduced_enemy_minions_box),color='red')
+        if enemy_minions != []:
+            e_m = randint(0,len(enemy_minions)-1) #attack a random taunt minion
+        else:
+            e_m = []
         if enemy != [] and enemy != None:
             actions.move_and_leftclick(c(defines.opponent_hero))
         elif enemy_minions != [] and enemy_minions != None:
-            actions.move_and_leftclick(enemy_minions[0])
+            actions.move_and_leftclick(enemy_minions[e_m])
         else:
-            actions.pause_pensively(3)
+            actions.pause_pensively(6)
         actions.move_and_leftclick(c(defines.neutral_minion))
         actions.pause_pensively(0.50)
         src = vision.screen_cap()
@@ -124,7 +126,7 @@ def player():
     player_ability = vision.color_range_reduced_mids(src,c(defines.reduced_ability_box),color='green')
     player_minions = vision.color_range_reduced_mids(src,c(defines.reduced_player_minions_box),color='green',threshold=45)
 
-    if (player_cards==[] and player_ability ==[] and player_minions ==[]) or player_cards==None or player_ability == None or player_minions == None or attack_attempt == 2:
+    if (player_cards==[] and player_ability ==[] and player_minions ==[]) or player_cards==None or player_ability == None or player_minions == None:
         #logging.info("---END TURN---")
         actions.move_and_leftclick(c(defines.turn_button))
         actions.move_and_leftclick(c(defines.turn_button))
@@ -162,11 +164,16 @@ def c(var):
     return defines.convert(var,defines.ref)
 
 def main():
-    global src
+    global src,character_sigs,state_sigs,stage_sigs
     global NEW_GAME
     new_state=0
     old_state=0
-    sigs = vision.get_sigs(os.getcwd()+ '\\images\\state\\')
+    def _window_callback(hwnd, all_windows):
+        all_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
+
+    state_sigs = vision.get_sigs(os.getcwd()+ '\\images\\state\\')
+    character_sigs = vision.get_sigs(os.getcwd()+ '\\images\\character\\')
+    #stage_sigs = vision.get_sigs(os.getcwd()+ '\\images\\stage\\')
 
     logging.basicConfig(filename='game.log',level=logging.DEBUG)
     logging.info("####################################")
@@ -176,46 +183,29 @@ def main():
     desktop_counter = 0
     while(True):
         src = vision.screen_cap()
-
-        new_state = defines.state_dict[vision.get_state(src,sigs)]        
+    
+        new_state = defines.state_dict[vision.get_state(src,state_sigs)]    
+     
         if new_state == old_state and new_state == defines.State.PLAY:
             #Might have been a connection error.
             actions.move_and_leftclick(c(defines.error));
             actions.move_and_leftclick(c(defines.neutral));
-
+    
         states[new_state]()
         if new_state != defines.State.DESKTOP:
-            desktop_counter =0
             actions.move_and_leftclick(c(defines.neutral));
         else:
-            #on the desktop for some reason, try to start the game going again
-            desktop_counter +=1
+            #on the desktop for some reason, try to start the game or reshow the window if it's already running
             actions.pause_pensively(10)
-            actions.move_and_leftclick(c(defines.hs_startbar_icon));
-            if desktop_counter == 3:
-                actions.move_and_leftclick(c(defines.blizzard_startbar_icon));
+            hwnd =  win32gui.FindWindow(0, "Hearthstone")
+            if hwnd != None and hwnd != 0:
+                win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
+                actions.pause_pensively(5)
+            else:
                 actions.move_and_leftclick(c(defines.blizzard_hs_play_button));
-                desktop_counter =0
+                actions.pause_pensively(10)
+            
         old_state=new_state
 
 if __name__ == '__main__':
     main()
-
-#src = vision.screen_cap()
-#
-#vision.screen_save()
-#src = vision.screen_load()
-#src = vision.imread(os.getcwd() + '\\temp\\attack3.png')
-#print defines.enemy_box
-#print defines.convert(defines.enemy_box,defines.ref)
-#print "game state        : ",defines.state_dict[vision.get_state(src)]
-#print "opponent char     : ",vision.get_image_info('character',src,defines.enemy_box)
-#print "player char       : ",vision.get_image_info('character',src,defines.player_box)
-#print "stage             : ",vision.get_image_info('stage',src,defines.stage_box)
-#print "turn              : ",vision.get_image_info('turn',src,defines.turn_box)
-#print "playable cards    : ",vision.get_playable_cards(src,defines.hand_box)
-#print "player            : ",vision.color_range_reduced_mids(src,defines.reduced_player_box,color='green')
-#print "player ability    : ",vision.color_range_reduced_mids(src,defines.reduced_ability_box,color='green')
-#print "player minions    : ",player_minions = vision.color_range_reduced_mids(src,c(defines.reduced_player_minions_box),color='green',threshold=45)
-#print "enemy             : ",vision.color_range_reduced_mids(src,defines.reduced_opponent_box,color='red')
-#print "enemy minions     : ",vision.color_range_reduced_mids(src,defines.reduced_enemy_minions_box,color='red')
