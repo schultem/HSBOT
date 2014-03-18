820b8a715887e445adcfd7205c4e9bbb6d6abf60
mjs <schulte.watt@gmail.com>
2014-03-17 21:44:59 -0600

    restart_game now handles launch error messages too.  check_game just returns if the game is running.

--- actions.py
+++ actions.py
@@ -98,38 +98,60 @@ def whnds_to_text(hwnds):
         hwnds[hwnd]=win32gui.GetWindowText(hwnds[hwnd])
     return hwnds
 
+def print_all_whnds(hwnds):
+    hwnds=[]
+    def enumHandler(hwnd, lParam):
+        hwnds.append(hwnd)
+
+    win32gui.EnumWindows(enumHandler, None)
+    text_hwnds = whnds_to_text(hwnds)
+    print text_hwnds
+
+#Return True if game is not minimized and is running.  Otherwise False
+def check_game():
+    whndl =  get_whndl("Hearthstone")
+    if whndl != None and whndl != 0:
+        #win32gui.IsWindowVisible(whndl)
+        return True
+    return False
+
 def restart_game():
-    pause_pensively(10)
     whndl =  get_whndl("Hearthstone")
     if whndl != None and whndl != 0:
         win32gui.ShowWindow(whndl, win32con.SW_MAXIMIZE)
         pause_pensively(10)
     else:
-        whndl  = get_whndl("Battle.net")
-        if whndl != None and whndl != 0:
-            #prepare battlenet window for input
-            win32gui.ShowWindow(whndl, win32con.SW_MAXIMIZE)
-            foreground_whndl(whndl)
-            pycwnd = make_pycwnd(whndl)
-
-            #try to log on again
-            pycwnd_click(pycwnd,defines.c(defines.bnet_go_online_button))
-            whndl_login = get_whndl("Battle.net Login")
-            if whndl_login != None and whndl_login != 0:
-                #prepare battlenet login window for input
-                pycwnd_login = make_pycwnd(whndl_login)
-                pycwnd_string(pycwnd_login,pw)
-                pycwnd_click(pycwnd_login,defines.bnet_accept_pw_button)
+        whndl_error = get_whndl("Battle.net Error")
+        if whndl_error == None or whndl_error == 0:
+            whndl  = get_whndl("Battle.net")
+            if whndl != None and whndl != 0:
+                #prepare battlenet window for input
+                win32gui.ShowWindow(whndl, win32con.SW_MAXIMIZE)
+                foreground_whndl(whndl)
+                pycwnd = make_pycwnd(whndl)
+            
+                #try to log on again
+                pycwnd_click(pycwnd,defines.c(defines.bnet_go_online_button))
+                whndl_login = get_whndl("Battle.net Login")
+                if whndl_login != None and whndl_login != 0:
+                    #prepare battlenet login window for input
+                    pycwnd_login = make_pycwnd(whndl_login)
+                    pycwnd_string(pycwnd_login,pw)
+                    pycwnd_click(pycwnd_login,defines.bnet_accept_pw_button)
+                    pause_pensively(5)
+            
+                #try to start the game
+                pycwnd_click(pycwnd,defines.c(defines.bnet_hearthstone_button))
+                pycwnd_click(pycwnd,defines.c(defines.bnet_play_button))
+            
+                #get battlenet out of the way
                 pause_pensively(5)
-
-            #try to start the game
-            pycwnd_click(pycwnd,defines.c(defines.bnet_hearthstone_button))
-            pycwnd_click(pycwnd,defines.c(defines.bnet_play_button))
-
-            #get battlenet out of the way
-            pause_pensively(5)
-            win32gui.ShowWindow(whndl, win32con.SW_MINIMIZE)
-            pause_pensively(15)
+                win32gui.ShowWindow(whndl, win32con.SW_MINIMIZE)
+                pause_pensively(20)
+        else:
+            foreground_whndl(whndl_error)
+            pycwnd_error = make_pycwnd(whndl_error)
+            pycwnd_click(pycwnd_error,defines.bnet_launch_error_button)
 
 def main():
     pass
