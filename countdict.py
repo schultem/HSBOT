25cea8a050099c8616962d02ab37a08a79f6dc85
mjs <schulte.watt@gmail.com>
2014-04-22 00:31:00 -0600

    Return invalid number -1 on failure, add optional mode limit

--- countdict.py
+++ countdict.py
@@ -1,8 +1,10 @@
 #return the mode of a dict
-def countdict(items):
+def countdict(items,limit=1):
     d = {}
     max_i=0
     for i in items:
+        if i == '':
+            i='blank'
         if i in d:
             d[i] = d[i]+1
             if d[i]>max_i:
@@ -11,4 +13,7 @@ def countdict(items):
             d[i] = 1
             if d[i]>max_i:
                 max_i=i
-    return i
+    if d[i]>=limit:            
+        return i
+    else:
+        return -1
