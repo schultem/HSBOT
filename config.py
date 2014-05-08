24fd7223558f183f342424cc30b40160e8c9f90d
mjs <schulte.watt@gmail.com>
2014-05-07 20:16:18 -0600

    Factoring out config input output to separate file.

---
+++ config.py
@@ -0,0 +1,72 @@
+import defines
+
+def save_config():
+    f = open( 'config.txt', 'w' )
+    f.write( 'DECKS_TO_USE          = ' + repr(defines.DECKS_TO_USE)       + '\n' )
+    f.write( 'REROLL_QUESTS         = ' + str(int(defines.REROLL_QUESTS))  + '\n' )
+    f.write( 'MULLIGAN              = ' + str(int(defines.MULLIGAN))       + '\n' )
+    f.write( 'RANDOM_ATTACKS        = ' + str(int(defines.RANDOM_ATTACKS)) + '\n' )
+    f.write( 'USE_MOUSE             = ' + str(int(defines.USE_MOUSE))      + '\n' )
+    f.write( 'PLAY_RANKED           = ' + str(int(defines.PLAY_RANKED))    + '\n' )
+    f.write( 'MOUSE_SPEED           = ' + str(int(defines.MOUSE_SPEED))    + '\n' )
+    f.write( 'START_HOUR            = ' + str(int(defines.START_HOUR))     + '\n' )
+    f.write( 'STOP_HOUR             = ' + str(int(defines.STOP_HOUR))      + '\n' )
+    f.close()
+
+def load_config():
+    config_file = open( 'config.txt', 'r' )
+    chars = config_file.readline()
+    defines.DECKS_TO_USE = []
+    for ch in chars:
+        if ch.isdigit():
+            defines.DECKS_TO_USE.append(int(ch))
+
+    chars = config_file.readline()
+    for ch in chars:
+        if ch.isdigit():
+            defines.REROLL_QUESTS   = int(ch)
+
+    chars = config_file.readline()
+    for ch in chars:
+        if ch.isdigit():
+            defines.MULLIGAN        = int(ch)
+
+    chars = config_file.readline()
+    for ch in chars:
+        if ch.isdigit():
+            defines.RANDOM_ATTACKS  = int(ch)
+
+    chars = config_file.readline()
+    for ch in chars:
+        if ch.isdigit():
+            defines.USE_MOUSE  = int(ch)
+
+    chars = config_file.readline()
+    for ch in chars:
+        if ch.isdigit():
+            defines.PLAY_RANKED  = int(ch)
+
+    chars = config_file.readline()
+    for ch in chars:
+        if ch.isdigit():
+            defines.MOUSE_SPEED  = int(ch)
+    
+    chars = config_file.readline()
+    total_ch=''
+    for ch in chars:
+        if ch.isdigit():
+            total_ch+=ch
+        else:
+            total_ch+=''
+    defines.START_HOUR  = int(total_ch)
+
+    chars = config_file.readline()
+    total_ch=''
+    for ch in chars:
+        if ch.isdigit():
+            total_ch+=ch
+        else:
+            total_ch+=''
+    defines.STOP_HOUR  = int(total_ch)
+
+    config_file.close()
