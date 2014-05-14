87f4fa4577a14b07f52f80b6211f1cb5c84a55d6
mjs <schulte.watt@gmail.com>
2014-05-13 21:32:44 -0600

    Trying to include x86 dlls

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from distutils.core import setup
 import py2exe
+from glob import glob
 
-setup(console=['play.py'])
+data_files = [("VC90", glob(r'C:\Windows\winsxs\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.21022.8_none_bcb86ed6ac711f91\*.*')),
+            ("VC90", glob(r'C:\Windows\winsxs\Manifests\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.21022.8_none_bcb86ed6ac711f91.manifest'))
+]
+
+setup(
+    data_files=data_files,
+    console=['play.py'],
+    zipfile = None,
+)
